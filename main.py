421825c8a67014cf261b0c5239fe96acd6a36ea6
Mariana <marianargdy@gmail.com>
2023-02-15 17:16:49 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+# TERCEIRO TRABALHO PARA A DISCIPLINA DE VISÃO COMPUTACIONAL 
+# Reconstrução da posição 3D durante o movimento de um robô móvel
+
+# Breno Ferreira e Mariana Godoy
+
+
+# Bibliotecas usadas
+
+
+
+
+# Funções 
+
+
+
+
+# Main
+
+
+# 1º: 
