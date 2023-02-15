2e54443e7c06760154e84e6bb5a3b4cd0d4ff178
harmonia2side <breno_odnan@hotmail.com>
2023-02-15 17:37:24 -0300

    Adiciona main.py com cabeçalho inicial

---
+++ main.py
@@ -0,0 +1,12 @@
+# TRABALHO 3 DE VISÃO COMPUTACIONAL
+# Nome: Breno Ferreira e Mariana Godoy
+
+# Reconstrução da posição 3D durante o movimento de um robô móvel
+
+# Neste trabalho vocês deverão detectar o robô nos vídeos das 4 câmeras do 
+# espaço inteligente e obter a reconstrução da sua posição 3D no mundo. 
+# Feito isso, vocês deverão gerar um gráfico da posição do robô, 
+# mostrando a trajetória que ele realizou.
+
+# ----- BIBLIOTECAS -----
+
